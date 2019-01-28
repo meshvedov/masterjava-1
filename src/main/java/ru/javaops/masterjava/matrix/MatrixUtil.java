6b8fb3ded7f6806b0b28540aab4552778f1e1477
Mikhail Shvedov <micangel@mail.ru>
2019-01-28 16:48:32 +0300

    hw1

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 /**
  * gkislin
@@ -15,21 +16,67 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        int cores = Runtime.getRuntime().availableProcessors();
+        int range = matrixSize / cores;
+        final CompletionService<Integer> service = new ExecutorCompletionService<>(executor);
+
+        ArrayList<Future<Integer>> futures = new ArrayList<>();
+        int thatColumn[] = new int[matrixSize];
+        for (int j = 0; j < matrixSize; j++) {
+            for (int k = 0; k < matrixSize; k++) {
+                thatColumn[k] = matrixB[k][j];
+            }
+            for (int i = 0; i < matrixSize; i++) {
+                int thisRow[] = matrixA[i];
+                int summand = 0;
+                Future<Integer> future = service.submit(() -> mult(thisRow, thatColumn));
+                futures.add(future);
+                while (!futures.isEmpty()){
+                    Future<Integer> future1 = service.poll(1, TimeUnit.SECONDS );
+                    futures.remove(future1);
+                    summand += future1.get();
+                }
+                matrixC[i][j] = summand;
+            }
+        }
         return matrixC;
     }
 
+    private static int mult(int[] arrA, int[] arrB){
+        int sum = 0;
+        int arrSize = arrA.length;
+        for (int k = 0; k < arrSize; k++) {
+            sum += arrA[k] * arrB[k];
+        }
+        return sum;
+    }
     // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
+//        for (int i = 0; i < matrixSize; i++) {
+//            for (int j = 0; j < matrixSize; j++) {
+//                int sum = 0;
+//                for (int k = 0; k < matrixSize; k++) {
+//                    sum += matrixA[i][k] * matrixB[k][j];
+//                }
+//                matrixC[i][j] = sum;
+//            }
+//        }
+//        return matrixC;
+        int thatColumn[] = new int[matrixSize];
+        for (int j = 0; j < matrixSize; j++) {
+            for (int k = 0; k < matrixSize; k++) {
+                thatColumn[k] = matrixB[k][j];
+            }
+            for (int i = 0; i < matrixSize; i++) {
+                int thisRow[] = matrixA[i];
+                int summand = 0;
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    summand += thisRow[k] * thatColumn[k];
                 }
-                matrixC[i][j] = sum;
+                matrixC[i][j] = summand;
             }
         }
         return matrixC;
